387d1df38d60ea828660b749866804fbce97c161
annisarisma <annisarisma117@gmail.com>
2024-02-11 20:20:32 +0700

    Add: sign up pages section username add to widget ref #2

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -349,6 +349,7 @@ class SignUpPage extends StatelessWidget {
             children: [
               header(),
               nameInput(),
+              usernameInput(),
               emailInput(),
               passwordInput(),
               buttonSignIn(),
