a80ef38ab938e98fbbb06bd88f8cb44c0e07214e
annisarisma <annisarisma117@gmail.com>
2024-02-17 22:57:48 +0700

    Add: create home section header ref #4

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:footcraze_frontend/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Home Page',
-      ),
+
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          right: defaultMargin,
+          left: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello Alex',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold
+                    ),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 16
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/image_profile.png',
+                  ),
+                )
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header()
+      ],
     );
   }
 }
