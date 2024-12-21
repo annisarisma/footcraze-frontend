aadba6c37455dfc4021f6c8ea73dad7d5475c9c6
annisarisma <annisarisma117@gmail.com>
2024-12-21 22:05:18 +0700

    add: header in profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:footcraze_frontend/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile Page'
-      ),
+    
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgOneColor,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello, Alex',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold
+                        ),
+                      ),
+                      Text(
+                        '@alexkeinn',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/images/button_exit.png',
+                  width: 20,
+                )
+              ],
+            ),
+          )
+        ),
+      );
+    }
+    
+    return Column(
+      children: [
+        header(),
+        Text('asdasd', style: primaryTextStyle,)
+      ],
     );
   }
 }
