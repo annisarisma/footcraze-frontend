2bf7fb104a9686911e782453060d025b5d8eefd0
annisarisma <annisarisma117@gmail.com>
2024-12-21 15:34:16 +0700

    add: chat bubble and chat content product

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:footcraze_frontend/theme.dart';
+import 'package:footcraze_frontend/widget/chat_bubble.dart';
 
 class DetailChatPage extends StatelessWidget {
   const DetailChatPage({super.key});
@@ -48,9 +49,132 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget productReview(){
+      return Container(
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(
+          bottom: 20
+        ),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: bgFiveColor,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor
+          )
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/images/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISION COURT VISIONCOURT VISION',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57.16',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/images/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productReview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: bgFourColor,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/images/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin
+        ),
+        children: [
+          ChatBubble(
+            isSender: true,
+            text: 'Hi, This item is still available?'
+          ),
+          ChatBubble(
+            isSender: false,
+            text: 'Good night, This item is only available in size 42 and 43'
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgThreeColor,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
+      body: content(),
     );
   }
 }
