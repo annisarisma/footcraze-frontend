addff20f42c3266b8a91725b0edc9271f1a2d543
annisarisma <annisarisma117@gmail.com>
2024-12-21 22:29:09 +0700

    refactor: button logout route sign in

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -46,9 +46,17 @@ class ProfilePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                Image.asset(
-                  'assets/images/button_exit.png',
-                  width: 20,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context, '/sign-in',
+                      (route) => false
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/images/button_exit.png',
+                    width: 20,
+                  ),
                 )
               ],
             ),
